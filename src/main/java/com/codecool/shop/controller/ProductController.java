5a108cbc4a5394af85eb33debc439c03c3d88582
Lizi Boros <boroslizi@gmail.com>
2019-05-15 13:53:23 +0200

    Filter refactored

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -26,13 +26,45 @@ public class ProductController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        ProductDao productDataStore = ProductDaoMem.getInstance();
         ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
         SupplierDao supplierDao = SupplierDaoMem.getInstance();
 
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
 
+        ProductCategory category = getProductCategory(req, productCategoryDataStore);
+        Supplier supplier = getSupplier(req, supplierDao);
+
+        context.setVariable("category", category);
+        context.setVariable("categories", productCategoryDataStore.getAll());
+        context.setVariable("suppliers", supplierDao.getAll());
+        filterProducts(category, supplier, context);
+        engine.process("product/index.html", context, resp.getWriter());
+    }
+
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+
+    private void filterProducts(ProductCategory category, Supplier supplier, WebContext context) {
+        ProductDao productDataStore = ProductDaoMem.getInstance();
+        String defaultSupplier = "Default supplier";
+        String defaultProduct = "Products";
+
+        if (category.getName().equals(defaultProduct) && !supplier.getName().equals(defaultSupplier)) {
+            context.setVariable("products", productDataStore.getBy(supplier));
+        } else if (!category.getName().equals(defaultProduct) && supplier.getName().equals(defaultSupplier)) {
+            context.setVariable("products", productDataStore.getBy(category));
+        } else if (category.getName().equals(defaultProduct) && supplier.getName().equals(defaultSupplier)) {
+            context.setVariable("products", productDataStore.getAll());
+        } else {
+            context.setVariable("products", productDataStore.getBy(supplier, category));
+        }
+
+    }
+
+    private ProductCategory getProductCategory(HttpServletRequest req, ProductCategoryDao productCategoryDataStore) {
         ProductCategory category;
 
         String categoryId = (req.getParameter("category") != null) ? req.getParameter("category") : "";
@@ -41,7 +73,10 @@ public class ProductController extends HttpServlet {
         } catch (NumberFormatException e) {
             category = new ProductCategory("Products", "Default", "Default description");
         }
+        return category;
+    }
 
+    private Supplier getSupplier(HttpServletRequest req, SupplierDao supplierDao) {
         Supplier supplier;
 
         String supplierId = (req.getParameter("supplier") != null) ? req.getParameter("supplier") : "";
@@ -50,27 +85,7 @@ public class ProductController extends HttpServlet {
         } catch (NumberFormatException e) {
             supplier = new Supplier("Default supplier", "Default state", "Default city", "Default description");
         }
-
-        context.setVariable("category", category);
-        context.setVariable("categories", productCategoryDataStore.getAll());
-        context.setVariable("suppliers", supplierDao.getAll());
-
-        if (category.getName().equals("Products") && !supplier.getName().equals("Default supplier")) {
-            context.setVariable("products", productDataStore.getBy(supplier));
-        } else if (!category.getName().equals("Products") && supplier.getName().equals("Default supplier")) {
-            context.setVariable("products", productDataStore.getBy(category));
-        } else if (category.getName().equals("Products") && supplier.getName().equals("Default supplier")) {
-            context.setVariable("products", productDataStore.getAll());
-        } else {
-            context.setVariable("products", productDataStore.getBy(supplier, category));
-        }
-
-        engine.process("product/index.html", context, resp.getWriter());
-    }
-
-
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        doGet(req, resp);
+        return supplier;
     }
 
 }
