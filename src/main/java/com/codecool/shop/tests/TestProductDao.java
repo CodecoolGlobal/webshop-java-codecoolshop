0d6df214fcf5f12c96610bee01ad66e1722b4f0b
zoltanNemeth <nemeth.zoltan.elte@gmail.com>
2019-05-30 11:48:48 +0200

    ProductDaoTest corrected according to the refactored names of other objects

--- src/main/java/com/codecool/shop/tests/TestProductDao.java
+++ src/main/java/com/codecool/shop/tests/TestProductDao.java
@@ -1,12 +1,12 @@
 package com.codecool.shop.tests;
 
-import com.codecool.shop.dao.ProductDao;
-import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
-import com.codecool.shop.dao.implementation.ProductDaoMem;
-import com.codecool.shop.dao.implementation.SupplierDaoMem;
-import com.codecool.shop.model.Product;
-import com.codecool.shop.model.ProductCategory;
-import com.codecool.shop.model.Supplier;
+import com.codecool.shop.dao.AnimalDao;
+import com.codecool.shop.dao.implementation.AnimalDaoMem;
+import com.codecool.shop.dao.implementation.SpeciesDaoMem;
+import com.codecool.shop.dao.implementation.ZooDaoMem;
+import com.codecool.shop.model.Animal;
+import com.codecool.shop.model.Species;
+import com.codecool.shop.model.Zoo;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -16,43 +16,43 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestProductDao {
-    private ProductDao productDao;
+    private AnimalDao animalDao;
 
     /**
      * Make a new instance every time before running a test
      */
     @BeforeEach
     void setup() {
-        productDao = ProductDaoMem.getInstance();
+        animalDao = AnimalDaoMem.getInstance();
     }
 
     @Test
     void testGetBySupplier() {
-        Supplier supplier = SupplierDaoMem.getInstance().find(1);
-        List<Product> expected = new ArrayList<>();
-        expected.add(productDao.find(12));
-        expected.add(productDao.find(24));
-        expected.add(productDao.find(36));
-        expected.add(productDao.find(48));
-        assertEquals(productDao.getBy(supplier), expected);
+        Zoo zoo = ZooDaoMem.getInstance().find(1);
+        List<Animal> expected = new ArrayList<>();
+        expected.add(animalDao.find(12));
+        expected.add(animalDao.find(24));
+        expected.add(animalDao.find(36));
+        expected.add(animalDao.find(48));
+        assertEquals(animalDao.getBy(zoo), expected);
     }
 
     @Test
     void testGetByProductCategory() {
-        ProductCategory productCategory = ProductCategoryDaoMem.getInstance().find(1);
-        List<Product> expected = new ArrayList<>();
-        expected.add(productDao.find(1));
-        expected.add(productDao.find(2));
-        expected.add(productDao.find(3));
-        expected.add(productDao.find(4));
-        expected.add(productDao.find(5));
-        expected.add(productDao.find(6));
-        expected.add(productDao.find(7));
-        expected.add(productDao.find(8));
-        expected.add(productDao.find(9));
-        expected.add(productDao.find(10));
-        expected.add(productDao.find(11));
-        expected.add(productDao.find(12));
-        assertEquals(productDao.getBy(productCategory), expected);
+        Species productCategory = SpeciesDaoMem.getInstance().find(1);
+        List<Animal> expected = new ArrayList<>();
+        expected.add(animalDao.find(1));
+        expected.add(animalDao.find(2));
+        expected.add(animalDao.find(3));
+        expected.add(animalDao.find(4));
+        expected.add(animalDao.find(5));
+        expected.add(animalDao.find(6));
+        expected.add(animalDao.find(7));
+        expected.add(animalDao.find(8));
+        expected.add(animalDao.find(9));
+        expected.add(animalDao.find(10));
+        expected.add(animalDao.find(11));
+        expected.add(animalDao.find(12));
+        assertEquals(animalDao.getBy(productCategory), expected);
     }
 }
