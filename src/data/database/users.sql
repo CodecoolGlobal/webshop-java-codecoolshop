168c241c26f00cdb31d70e9e487dcef5d661ecab
Erika Toth <erikatoth.0831@gmail.com>
2019-05-30 13:56:41 +0200

    users sql

---
+++ src/data/database/users.sql
@@ -0,0 +1,8 @@
+drop table if exists users;
+
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL UNIQUE PRIMARY KEY,
+    "name" VARCHAR(255),
+    "email" VARCHAR(255),
+    "password" VARCHAR(255),
+);
